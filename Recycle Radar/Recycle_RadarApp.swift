e7b59d33a7005037208eb0f3922087f89f3c97d8
victorxiao <victor2rooster@gmail.com>
2021-12-01 10:26:36 -0800

    Initial Commit

---
+++ Recycle Radar/Recycle_RadarApp.swift
@@ -0,0 +1,17 @@
+//
+//  Recycle_RadarApp.swift
+//  Recycle Radar
+//
+//  Created by Victor Xiao on 12/1/21.
+//
+
+import SwiftUI
+
+@main
+struct Recycle_RadarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
